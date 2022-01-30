f27a58678bdc90495cf8a0981f3253e654728d51
bonannogiovanni <bonannogiovanni@hotmail.com>
2022-01-30 18:41:48 +0100

    recipe app

---
+++ studio/schemas/ingredient.js
@@ -0,0 +1,21 @@
+const ingredient = {
+  name: "ingredient",
+  title: "Ingredient",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Ingredient Name",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: { hotspot: true },
+    },
+    { name: "notes", title: "Notes", type: "text" },
+  ],
+};
+
+export default ingredient;
